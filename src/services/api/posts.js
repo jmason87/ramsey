7a19e0ba696a2c7774906b6cc43afcf7dca215a9
Joe Mason <joewebdev87@gmail.com>
2025-11-05 09:54:38 -0600

    add CRUD posts api calls

--- src/services/api/posts.js
+++ src/services/api/posts.js
@@ -3,3 +3,32 @@ import { apiCall } from './index';
 export const getPosts = async () => {
   return apiCall('/posts/');
 };
+
+export const createPost = async (communityId, title, content, postType) => {
+  return apiCall('/posts/', {
+    method: 'POST',
+    body: JSON.stringify({ 
+      community_id: communityId,
+      title, 
+      content, 
+      post_type: postType 
+    }),
+  });
+};
+
+export const updatePost = async (postId, title, content, postType) => {
+  return apiCall(`/posts/${postId}/`, {
+    method: 'PUT',
+    body: JSON.stringify({ 
+      title, 
+      content, 
+      post_type: postType 
+    }),
+  });
+};
+
+export const deletePost = async (postId) => {
+  return apiCall(`/posts/${postId}/`, {
+    method: 'DELETE',
+  });
+};
